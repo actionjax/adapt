d3ee55c83d1fad92d9f391198612f6ef6f9e2812
actionjax <jackson.kwok@gmail.com>
2015-01-18 22:36:45 -0500

    added bar chart

--- generatecsv.py
+++ generatecsv.py
@@ -5,16 +5,32 @@ import datetime
 def generate(name):
 	with open(name, 'wb') as csvfile:
 		writer = csv.writer(csvfile)
-		writer.writerow(['dtoi','mean','low','high'])
+		writer.writerow(['dtoi','mean','low','high','obslow','obshigh'])
+		random_walk = 5
 		for x in range(0,365):
 			d = datetime.datetime(2015, 1, 1, 0, 0, 0)
 			d = d + datetime.timedelta(days=x)
-			mean = int(round(random.gauss(4,2),0))
+
+
+			mean = int(round(random.gauss(5,4),0))
+			variance = random.randint(0,4)
+			omi = mean-variance
+			oma = mean+variance
+
+			walk = int(round(random.gauss(0,4),0))
+			walk = random_walk + walk
+			if walk < 0:
+				walk = int(round(random.gauss(4,4),0))
+				walk = random_walk + walk
+			if walk > 10:
+				walk = int(round(random.gauss(-4,4),0))
+				walk = random_walk + walk
+
+			random_walk = walk
 			variance = random.randint(0,4)
 			mi = mean-variance
 			ma = mean+variance
 			d.strftime("%Y-%m-%dT%H:%M:%S")
-			writer.writerow([d,mean,mi,ma])
+			writer.writerow([d,mean,mi,ma,omi,oma])
 
-generate('data/adapt_observation.csv')
 generate('data/adapt_prediction.csv')
