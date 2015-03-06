911fa750e948188cab4a64fc6f8899fd2704005b
Jackson Kwok <jackson.kwok@gmail.com>
2015-03-05 20:24:07 -0500

    olympics

--- main/routes/index.js
+++ main/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { adapt_csv: 'data/adapt_prediction10days.csv' });
 });
 
+router.get('/big', function(req, res, next) {
+  res.render('index', { adapt_csv: 'data/adapt_prediction.csv' });
+});
+
+router.get('/olympics', function(req, res, next) {
+  res.render('olympics', { });
+});
+
 module.exports = router;
